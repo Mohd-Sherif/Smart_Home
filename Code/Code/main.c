13b778cc99c0107c329a7c91fb74548a5ecee937
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-04-29 02:25:13 +0300

    Cont. Init

---
+++ Code/Code/main.c
@@ -0,0 +1,18 @@
+/*
+ * Code.c
+ *
+ * Created: 29-Apr-23 2:21:42 AM
+ * Author : Mohamed Sherif
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
