07f575b9224a91c6ba1f2052bee00b35d27d12a5
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-04-29 19:07:36 +0300

    Cont. Init

---
+++ SlaveCode/SlaveCode/main.c
@@ -0,0 +1,18 @@
+/*
+ * SlaveCode.c
+ *
+ * Created: 29-Apr-23 7:06:20 PM
+ * Author : Mohamed Sherif
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
