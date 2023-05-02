0af004717845c98670d5d4cbdc6ecaa2a8dda9f4
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-05-02 20:26:10 +0300

    Adding menu functions .c and .h files

---
+++ Code/Code/SystemFunctions/menu.c
@@ -0,0 +1,9 @@
+/*
+ * menu.c
+ *
+ * Created: 29-Apr-23 6:19:19 AM
+ *  Author: Mohamed Sherif
+ */ 
+
+#include "../Config.h"
+
---
+++ Code/Code/SystemFunctions/menu.h
@@ -0,0 +1,11 @@
+/*
+ * menu.h
+ *
+ * Created: 29-Apr-23 6:19:08 AM
+ *  Author: Mohamed Sherif
+ */ 
+
+
+#ifndef MENU_H_
+#define MENU_H_
+#endif /* MENU_H_ */
