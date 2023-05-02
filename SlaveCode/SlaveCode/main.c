6afe2c4d9aa033c1e73f646bb4b93fdff604ab7d
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-05-02 20:19:26 +0300

    Adding Room1 request's respond

--- SlaveCode/SlaveCode/main.c
+++ SlaveCode/SlaveCode/main.c
@@ -28,6 +28,23 @@ int main(void)
 		request = SPI_Slave_Receive_char(DUMMY_DATA);
 		switch(request){
 			case ROOM1:
+				if(LED_u8read(ROOM1_LED_PORT, ROOM1_LED_PIN) == 1){
+					response = ON;
+				}
+				else{
+					response = OFF;
+				}
+				SPI_Slave_Receive_char(response);
+				request = SPI_Slave_Receive_char(DUMMY_DATA);
+				_delay_ms(TRANSMISSION_DELAY);
+				switch(request){
+					case ON:
+						LED_vON(ROOM1_LED_PORT, ROOM1_LED_PIN);
+						break;
+					case OFF:
+						LED_vOFF(ROOM1_LED_PORT, ROOM1_LED_PIN);
+						break;
+				}
 				break;
 			case ROOM2:
 				break;
