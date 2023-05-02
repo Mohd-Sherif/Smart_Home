3a080d3a042aa8b12f0568324890a4110421373a
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-05-02 19:57:10 +0300

    Including MCAL Drivers Headers

--- Code/Code/Config.h
+++ Code/Code/Config.h
@@ -9,4 +9,11 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
+// MCAL Drivers
+#include "Drivers/MCAL/StandardMacrosHeader/std_macros.h"
+#include "Drivers/MCAL/DioDriver/DIO.h"
+#include "Drivers/MCAL/EepromDriver/EEPROM.h"
+#include "Drivers/MCAL/AdcDriver/ADC.h"
+#include "Drivers/MCAL/TimerDriver/Timer.h"
+#include "Drivers/MCAL/SpiDriver/SPI.h"
 #endif /* CONFIG_H_ */
