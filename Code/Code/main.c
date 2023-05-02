4c920f685a11066486715167ddbe1b84a1f9eba9
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-05-02 20:04:35 +0300

    Initializing LCD

--- Code/Code/main.c
+++ Code/Code/main.c
@@ -8,5 +8,13 @@
 #include "Config.h"
 
 int main(void)
+{	
+    LCD_vinit();
+	LCD_vsend_string("Welcome to Shefo");
+	LCD_vmove_cursor(2, 1);
+	LCD_vsend_string("Smart Home");
+	_delay_ms(LCD_DELAY);
+	LCD_vCLR_screen();
+
     }
 }
